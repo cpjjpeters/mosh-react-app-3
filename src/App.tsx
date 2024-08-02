a222816311e2cb40f7e4b1019ee709af6b90a2f3
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-08-02 08:53:37 +0200

    working expenseList, no filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+// import "./App.css";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Car Insurance", amount: 294.67, category: "Auto" },
+    { id: 2, description: "Mortgage", amount: 1000, category: "Home" },
+    { id: 3, description: "Electricity", amount: 100, category: "Utilities" },
+    { id: 4, description: "Water", amount: 50, category: "Utilities" },
+  ]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ExpenseList
+          expenses={expenses}
+          onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
